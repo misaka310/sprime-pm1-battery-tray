58ce1f458afb3e416c2a96de8a5d593b318715f0
misaka310 <136806419+misaka310@users.noreply.github.com>
2026-07-22 16:38:19 +0900

    fix: tolerate missing startup registry key

--- src/sprime_pm1_battery_tray/startup.py
+++ src/sprime_pm1_battery_tray/startup.py
@@ -1,34 +1,39 @@
-import winreg
 import os
 import sys
+import winreg
 
 APP_NAME = "SPRIME PM1 Battery Tray"
+RUN_KEY = r"Software\Microsoft\Windows\CurrentVersion\Run"
+
 
 def is_startup_enabled():
     try:
-        key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"Software\Microsoft\Windows\CurrentVersion\Run", 0, winreg.KEY_READ)
-        value, _ = winreg.QueryValueEx(key, APP_NAME)
-        winreg.CloseKey(key)
+        with winreg.OpenKey(winreg.HKEY_CURRENT_USER, RUN_KEY, 0, winreg.KEY_READ) as key:
+            winreg.QueryValueEx(key, APP_NAME)
         return True
-    except WindowsError:
+    except OSError:
         return False
 
+
 def set_startup(enable):
-    key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"Software\Microsoft\Windows\CurrentVersion\Run", 0, winreg.KEY_SET_VALUE)
-    if enable:
-        if getattr(sys, 'frozen', False):
-            # Running as PyInstaller executable
-            exe_path = sys.executable
-            cmd = f'"{exe_path}"'
-        else:
-            # Running as python script
-            script_path = os.path.abspath(sys.argv[0])
-            python_path = sys.executable
-            cmd = f'"{python_path}" "{script_path}"'
-        winreg.SetValueEx(key, APP_NAME, 0, winreg.REG_SZ, cmd)
-    else:
+    if not enable:
         try:
-            winreg.DeleteValue(key, APP_NAME)
-        except WindowsError:
+            with winreg.OpenKey(winreg.HKEY_CURRENT_USER, RUN_KEY, 0, winreg.KEY_SET_VALUE) as key:
+                try:
+                    winreg.DeleteValue(key, APP_NAME)
+                except OSError:
+                    pass
+        except OSError:
             pass
-    winreg.CloseKey(key)
+        return
+
+    if getattr(sys, "frozen", False):
+        exe_path = sys.executable
+        cmd = f'"{exe_path}"'
+    else:
+        script_path = os.path.abspath(sys.argv[0])
+        python_path = sys.executable
+        cmd = f'"{python_path}" "{script_path}"'
+
+    with winreg.CreateKeyEx(winreg.HKEY_CURRENT_USER, RUN_KEY, 0, winreg.KEY_SET_VALUE) as key:
+        winreg.SetValueEx(key, APP_NAME, 0, winreg.REG_SZ, cmd)
